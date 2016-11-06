b55bb23d61487e822d8634c79d707b2d7835c0ee
gsotorrio <willybosko@gmail.com>
2016-11-06 17:48:10 +0100

    create function to see players

--- CrudWithJquery/CrudWithJquery/Scripts/App/managerPlayers.js
+++ CrudWithJquery/CrudWithJquery/Scripts/App/managerPlayers.js
@@ -1,1 +1,11 @@
-﻿
+﻿$(function () {
+    console.log("ready!");
+
+    $.get("http://localhost:13503/api/players", function (data) {
+        console.log(data);
+
+        $.each(data, function (index, player) {
+            $('#tbPlayers tr:last').after('<tr><td>' + player.PlayerId + '</td><td>' + player.Name + '</td><td>' + player.Surname + '</td><td>' + player.Position + '</td><td>' + player.StrongLeg + '</td><td>' + player.Age + '</td><td>' + player.PlayerNumber + '</td></tr>');
+        });       
+    });
+});
