89437c68235b9b4bdf8826f6c337f308010e2c4f
gsotorrio <willybosko@gmail.com>
2016-11-17 15:23:57 +0100

    traying do dynamic table

--- CrudWithJquery/CrudWithJquery/Scripts/App/crud-javaScript.js
+++ CrudWithJquery/CrudWithJquery/Scripts/App/crud-javaScript.js
@@ -32,3 +32,55 @@
 }
 
 
+
+
+ajax("GET", "http://localhost:13503/api/players", null, function (data) {
+    console.log(data);
+
+    var table = document.createElement('table');
+    for (var i = 0; i < data.length; i++) {
+        var tr = document.createElement('tr');
+
+        var tdName = document.createElement('td');
+        var tdSurName = document.createElement('td');
+        var tdPosition = document.createElement('td');
+        var tdStrongLeg = document.createElement('td');
+        var tdAge = document.createElement('td');
+        var tdNumber = document.createElement('td');
+        var tdDelete = document.createElement('td');
+        var tdUpdate = document.createElement('td');
+
+        var name = document.createTextNode(player.name);
+        var surname = document.createTextNode(data.surname);
+        var position = document.createTextNode(data.position);
+        var strongLeg = document.createTextNode(data.strongLeg);
+        var age = document.createTextNode(data.age);
+        var number = document.createTextNode(data.playerNumber);
+        var clean = document.createTextNode("delete");
+        var update = document.createTextNode("update");
+
+        tdName.appendChild(name);
+        tdSurName.appendChild(surname);
+        tdPosition.appendChild(position);
+        tdStrongLeg.appendChild(strongLeg);
+        tdAge.appendChild(age);
+        tdNumber.appendChild(number);
+        tdDelete.appendChild(clean);
+        tdUpdate.appendChild(update);
+
+        tr.appendChild(tdName);
+        tr.appendChild(tdSurName);
+        tr.appendChild(tdPosition);
+        tr.appendChild(tdStrongLeg);
+        tr.appendChild(tdAge);
+        tr.appendChild(tdNumber);
+        tr.appendChild(tdDelete);
+        tr.appendChild(tdUpdate);
+
+        table.appendChild(tr);
+    }
+    document.body.appendChild(table);
+});
+
+
+
