6fd8d59649e2c05ff86ab9227bbf974354f17902
osotorrio <osotorrio@hotmail.com>
2017-01-08 14:48:00 +0000

    Get and delete

--- CrudWithJquery/CrudWithJquery/Scripts/App/crud-ko.js
+++ CrudWithJquery/CrudWithJquery/Scripts/App/crud-ko.js
@@ -1,1 +1,33 @@
-﻿
+﻿(function () {
+
+    // Variables
+    var players = ko.observableArray();
+
+    // Functions
+    var remove = function(player, event){
+        $.ajax({
+            url: "http://localhost:13503/api/players/" + player.playerId,
+            method: "DELETE"
+        }).done(function () {
+            players.remove(player);
+        });
+    };
+
+    // ViewModel
+    var viewModel = {
+        players: players,
+        remove: remove
+    };
+
+    // On initialize
+    $(function () {
+        console.log("ready!");
+
+        $.get("http://localhost:13503/api/players", function (data) {
+            console.log(data);
+
+            players(data);
+            ko.applyBindings(viewModel);
+        });
+    });
+})();
