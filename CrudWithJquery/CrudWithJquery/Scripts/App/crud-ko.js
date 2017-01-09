aa838e9f52518e86079f24d543399473a356e4cb
gsotorrio <willybosko@gmail.com>
2017-01-09 14:22:45 +0100

    bad version

--- CrudWithJquery/CrudWithJquery/Scripts/App/crud-ko.js
+++ CrudWithJquery/CrudWithJquery/Scripts/App/crud-ko.js
@@ -1,1 +1,22 @@
-﻿
+﻿function SaveDatas() {
+    this.playerId = ko.observable();
+    this.name = ko.observable();
+    this.surname = ko.observable();
+    this.position = ko.observable();
+    this.strongLeg = ko.observable();
+    this.age = ko.observable();
+    this.numberPlayer = ko.observable();
+}
+
+function AddPlayersInTable() {
+    var self = this;
+
+   this.arrayPlayers = ko.observableArray([    ]);
+
+    self.addNewRow = function () {
+        self.arrayPlayers.push(new SaveDatas());
+    }
+
+}
+
+ko.applyBindings(new AddPlayersInTable());
